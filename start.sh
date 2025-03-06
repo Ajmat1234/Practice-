9f38ad964bf4e960414c1348979a9b31a512af20
Ajmat <hunterboyz73770@gmail.com>
2025-03-06 23:49:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Indic NLP resources ko download aur set karo
-git clone https://github.com/anoopkunchukuttan/indic_nlp_resources.git
+# Indic NLP resources download karo
+git clone https://github.com/anoopkunchukuttan/indic_nlp_resources.git || echo "Already exists"
 
 # Environment variable set karo
 export INDIC_RESOURCES_PATH=$(pwd)/indic_nlp_resources
